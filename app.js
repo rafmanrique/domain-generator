184a85e2701e2d8ec66c2a321b57daaa60e862a4
Rafael Manrique <rafamanrique26@gmail.com>
2022-03-12 16:46:39 -0400

    Domain Generator Done

---
+++ app.js
@@ -0,0 +1,15 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+
+for (i of pronoun){ 
+    for (j of adj){
+        for (k of noun){
+            console.log(i+j+k+'.com');
+        } 
+    }
+}
+
+
+
